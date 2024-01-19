a175d568a6c57ed191378638ce1ca5b1363d8fde
GekiteniusY <genkichi.yogi@gmail.com>
2024-01-19 22:45:42 +0900

    TextFieldのコンポーネントを追加 emotionの導入

--- src/component/text-field/text-field.tsx
+++ src/component/text-field/text-field.tsx
@@ -1,5 +1,46 @@
+import { css } from "@emotion/react";
 import React from "react";
 
-export const TextField = () => {
-  return <div>TextField</div>;
+interface TextFieldProps {
+  htmlFor: string;
+  label: string;
+  inputType: string;
+  inputName: string;
+  id: string;
+  placeholder: string;
+  isRequired: boolean;
+}
+
+const style = css`
+  color: hotpink;
+`;
+
+export const TextField: React.FC<TextFieldProps> = ({
+  htmlFor,
+  label,
+  inputType,
+  inputName,
+  id,
+  placeholder,
+  isRequired,
+}) => {
+  return (
+    <>
+      <div></div>
+      <label
+        htmlFor={htmlFor}
+        className='block mb-2 text-sm font-medium text-gray-900'
+      >
+        {label}
+      </label>
+      <input
+        type={inputType}
+        name={inputName}
+        id={id}
+        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+        placeholder={placeholder}
+        required={isRequired}
+      />
+    </>
+  );
 };
