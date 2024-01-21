9c60781ced53367526bf4b1d88e5292253fbd521
GekiteniusY <genkichi.yogi@gmail.com>
2024-01-21 21:39:13 +0900

    TextFieldのCSS記述をEmotionに変更

--- src/component/text-field/text-field.tsx
+++ src/component/text-field/text-field.tsx
@@ -10,8 +10,44 @@ interface TextFieldProps {
   isRequired: boolean;
 }
 
-const style = css`
-  color: hotpink;
+// font-size: 0.875rem; text-sm
+// font-weight: medium; font-medium
+// color: #1a202c; ext-gray-900
+const TextFieldLabelCss = css`
+  display: block;
+  margin-bottom: 2px;
+  font-size: 0.875rem;
+  font-weight: medium;
+  color: #1a202c;
+`;
+
+// background-color: #f9fafb; /* bg-gray-50 */
+// border: 1px solid #d2d6dc; /* border-gray-300 */
+// color: #1a202c; /* text-gray-900 */
+// font-size: 0.875rem; /* text-sm */
+// border-radius: 0.375rem; /* rounded-lg */
+// focus: {
+//   outline: none;
+//   ring: 2px solid #3b82f6; /* focus:ring-blue-500 */
+//   border: 1px solid #3b82f6; /* focus:border-blue-500 */
+// }
+// display: block;
+// width: 100%; /* w-full */
+// padding: 0.625rem; /* p-2.5 */
+const TextFieldInputCss = css`
+  background-color: #f9fafb;
+  border: 1px solid #d2d6dc;
+  color: #1a202c;
+  font-size: 0.875rem;
+  border-radius: 0.375rem;
+  focus: {
+    outline: none;
+    ring: 2px solid #3b82f6;
+    border: 1px solid #3b82f6;
+  }
+  display: block;
+  width: 100%;
+  padding: 0.625rem;
 `;
 
 export const TextField: React.FC<TextFieldProps> = ({
@@ -28,7 +64,7 @@ export const TextField: React.FC<TextFieldProps> = ({
       <div></div>
       <label
         htmlFor={htmlFor}
-        className='block mb-2 text-sm font-medium text-gray-900'
+        css={TextFieldLabelCss}
       >
         {label}
       </label>
@@ -36,7 +72,7 @@ export const TextField: React.FC<TextFieldProps> = ({
         type={inputType}
         name={inputName}
         id={id}
-        className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5'
+        css={TextFieldInputCss}
         placeholder={placeholder}
         required={isRequired}
       />
