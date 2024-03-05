0c8e8ca6e21bcdfd78eaa858e8d13f00c261737e
GekiteniusY <genkichi.yogi@gmail.com>
2024-03-05 22:23:07 +0900

    TailwindCSSのサンプルページを追加 レイアウトごとのディレクトリを作成

---
+++ src/app/atomic_design/templates/1column/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OneColumn = () => {
+  return <div>Atomic Design</div>;
+};
+
+export default OneColumn;
---
+++ src/app/atomic_design/templates/2column/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TwoColumn = () => {
+  return <div>Atomic Design</div>;
+};
+
+export default TwoColumn;
