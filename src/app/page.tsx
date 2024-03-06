583d84f450728e54b180a47e0ff31d7ca7b47a4e
GekiteniusY <genkichi.yogi@gmail.com>
2024-03-06 21:35:58 +0900

    トップページのレイアウト作成に着手

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,42 @@
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'></main>
+    <>
+      {/* main header */}
+      <header>
+        <h1>Header</h1>
+      </header>
+
+      {/* navigation */}
+      <nav className='flex'>
+        <ul>
+          <li>Home</li>
+          <li>Atomic Design</li>
+          <li>Cache Strategy</li>
+          <li>Next.js Rendering Types</li>
+          <li>TailwindCSS</li>
+        </ul>
+      </nav>
+
+      {/* main content */}
+      <main className='block bg-gray-300'>
+        <div className='m-7 bg-cyan-700'>
+          <h2>Atomic Design</h2>
+          <p>Atomic Designのコンポーネントをまとめたページ</p>
+        </div>
+        <div className='m-7 bg-green-400'>
+          <h2>Cache Strategy</h2>
+          <p>
+            ReactおよびNext.js（App
+            Router）のキャッシュ戦略についてまとめたページ
+          </p>
+        </div>
+
+        <h2>Next.js Rendering Types</h2>
+        <h2>TailwindCSS</h2>
+
+        {/* aside contents */}
+        <aside>nothing</aside>
+      </main>
+    </>
   );
 }
---
+++ src/app/tailwindcss/layout/padding_margin/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const PaddingAndMargin = () => {
+  return (
+    <div>
+      PaddingAndMargin
+      <div className=''></div>
+    </div>
+  );
+};
+
+export default PaddingAndMargin;
