d9b2687d0a5150b8ce80f8cf0501c388d7b5c472
GekiteniusY <genkichi.yogi@gmail.com>
2024-03-11 23:02:45 +0900

    ヘッダーレイアウトをGithubを参考に修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,24 +2,26 @@ export default function Home() {
   return (
     <>
       {/* main header */}
-      <header className='flex justify-center items-center w-full h-20 bg-blue-300'>
-        <h1 className='bg-green-300'>This site is a personal playground.</h1>
+      <header className='block justify-center items-center w-full'>
+        <h1 className='bg-blue-300 h-10'>
+          This site is a personal playground.
+        </h1>
+
+        {/* navigation */}
+        <nav className='flex justify-start items-center h-10 bg-red-300'>
+          <ul className='flex'>
+            <li className='ml-1'>Home</li>
+            <li className='ml-4'>Atomic Design</li>
+            <li className='ml-4'>Cache Strategy</li>
+            <li className='ml-4'>Next.js Rendering Types</li>
+            <li className='ml-4'>TailwindCSS</li>
+          </ul>
+          <p className='ml-10 bg-slate-400'>なにかいれるかも</p>
+        </nav>
       </header>
 
-      {/* navigation */}
-      <nav className='flex p-3'>
-        <ul className='flex'>
-          <li className='ml-1'>Home</li>
-          <li className='ml-4'>Atomic Design</li>
-          <li className='ml-4'>Cache Strategy</li>
-          <li className='ml-4'>Next.js Rendering Types</li>
-          <li className='ml-4'>TailwindCSS</li>
-        </ul>
-        <p className='ml-10 bg-slate-400'>なにかいれるかも</p>
-      </nav>
-
       {/* aside contents */}
-      <aside className='mx-4'>
+      <aside className='mx-4 mt-4'>
         Notice
         <div>THIS SITE IS UNDER CONSTRUCTION</div>
         <ul className='block'>
@@ -31,7 +33,7 @@ export default function Home() {
 
       {/* main content */}
       <main className='block mx-4 bg-gray-300'>
-        <div className='mt-7 box-content bg-cyan-700'>
+        <div className='mt-7 box-content bg-green-400'>
           <h2>Atomic Design</h2>
           <p>Atomic Designのコンポーネントをまとめたページ</p>
         </div>
