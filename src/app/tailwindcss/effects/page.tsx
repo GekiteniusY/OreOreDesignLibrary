87fc01de84ab0a129946ddfeb7068fd976ec5631
GekiteniusY <genkichi.yogi@gmail.com>
2024-03-14 23:01:31 +0900

    TailwindCSSのページを追加

---
+++ src/app/tailwindcss/effects/page.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Effects = () => {
+  return (
+    <div className='block'>
+      Effects
+      <div>Box Shadow</div>
+      <div>Box Shadow Color</div>
+      <div>Opacity</div>
+      <div>
+        Mix Blend Mode
+        <p>要素が重なり合ったときのモード</p>
+      </div>
+      <div>Background Belnd Mode</div>
+    </div>
+  );
+};
+
+export default Effects;
--- src/app/tailwindcss/page.tsx
+++ src/app/tailwindcss/page.tsx
@@ -2,14 +2,25 @@ import Link from "next/link";
 
 const BoxDecorationBreak = () => {
   return (
-    <div>
-      <Link href={"/tailwindcss/layout/box_decoration_break"}>
-        box-decoration-break: 複数行要素のレンダリングオプション
-      </Link>
-      <p>
-        複数行（multiple
-        line）のコンテンツを含んだ場合、それぞれの行を個別にあつかうか、複数行で1つとして扱うか
-      </p>
+    <div className='block'>
+      <div>
+        <Link href={"/tailwindcss/layout"}>Layout</Link>
+      </div>
+      <div>
+        <Link href={"/tailwindcss/effects"}>Effects</Link>
+      </div>
+      <div>
+        <Link href={"/tailwindcss/animation"}>Animation</Link>
+      </div>
+      <div>
+        <Link href={"/tailwindcss/layout/box_decoration_break"}>
+          box-decoration-break: 複数行要素のレンダリングオプション
+        </Link>
+        <p>
+          複数行（multiple
+          line）のコンテンツを含んだ場合、それぞれの行を個別にあつかうか、複数行で1つとして扱うか
+        </p>
+      </div>
     </div>
   );
 };
