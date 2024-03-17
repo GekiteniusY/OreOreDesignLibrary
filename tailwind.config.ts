300dc309582628e346ef434f629f4ef26cfbf96a
GekiteniusY <genkichi.yogi@gmail.com>
2024-03-17 14:49:30 +0900

    TailwindCSSのconfigを修正

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,7 @@ const config: Config = {
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./layout/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./component/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
